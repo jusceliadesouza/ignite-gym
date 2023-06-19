f2389d8bec7232e6e722c12c4c1d6225c3e660bc
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-19 20:25:02 -0300

    :label: tipagem das rotas

--- mobile/src/routes/auth.routes.tsx
+++ mobile/src/routes/auth.routes.tsx
@@ -1,19 +1,28 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack'
+
 import { SignIn } from '@screens/SignIn'
 import { SignUp } from '@screens/SignUp'
 
-const { Navigator, Screen } = createNativeStackNavigator()
+
+type AuthRoutes = {
+  signIn: undefined
+  signUp: undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>()
 
 export function AuthRoutes() {
   return (
     <Navigator>
       <Screen
-        name="signin"
+        name="signIn"
         component={SignIn}
       />
 
       <Screen
-        name="signup"
+        name="signUp"
         component={SignUp}
       />
     </Navigator>

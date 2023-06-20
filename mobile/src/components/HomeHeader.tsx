1b3208d4c563d3e6ddcbc15696ebf9318c31665d
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-19 23:42:10 -0300

    :sparkles: feat: componente userphoto

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -1,14 +1,22 @@
 import { Heading, HStack, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack 
+    <HStack
       pt={16} 
       pb={5} 
       px={8}
       bg="gray.600" 
       alignItems="center"
     >
+      <UserPhoto 
+        source={{ uri: 'https://github.com/jusceliadesouza.png'}}
+        size={16}
+        mr={4}
+        alt="Imagem do usuário"
+      />
+      
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá, 

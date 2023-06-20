ae018f251d3b6d29017e409edc166ccdc0b39270
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-19 23:36:33 -0300

    :lipstick: feat: estilização do homeheader

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -2,13 +2,19 @@ import { Heading, HStack, Text, VStack } from "native-base";
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack 
+      pt={16} 
+      pb={5} 
+      px={8}
+      bg="gray.600" 
+      alignItems="center"
+    >
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá, 
         </Text>
         
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Juscélia
         </Heading>
       </VStack>

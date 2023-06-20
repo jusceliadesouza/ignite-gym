bb2403b7101a1add0ef88dafbf2e5db781d8b65e
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-19 23:48:29 -0300

    :sparkles: feat: finalização do homeheader

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -19,7 +19,7 @@ export function HomeHeader() {
         alt="Imagem do usuário"
       />
       
-      <VStack>
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá, 
         </Text>

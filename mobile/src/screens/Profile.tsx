6930cafd009d01177a7c12334f1b47ff7ef41669
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-21 01:16:18 -0300

    :sparkles: feat: input de nome e e-mail

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { TouchableOpacity } from 'react-native'
+import { Input } from '@components/Input'
 
 const PHOTO_SIZE = 33
 
@@ -43,6 +44,17 @@ export function Profile(){
               Alterar foto
             </Text>
           </TouchableOpacity>
+
+          <Input 
+            bg="gray.600"
+            placeholder="Nome"
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="jusceliadesousa@gmail.com"
+            isDisabled
+          />
         </Center>
       </ScrollView>
     </VStack>

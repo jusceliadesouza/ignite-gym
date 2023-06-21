47a311449724f105ce483b1ff23cc52dfdac9890
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-20 23:12:44 -0300

    :sparkles: feat: componente group

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,14 @@
+import { VStack } from 'native-base'
+
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Center, Text, VStack } from 'native-base'
 
 export function Home(){
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <Group name="costas" />
     </VStack>
   )
 }

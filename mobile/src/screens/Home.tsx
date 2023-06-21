d794634c1f1904ca2efd07aa3bb585f7a504c05d
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-20 23:41:36 -0300

    :sparkles: feat: header da lista de exercícios

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { FlatList, HStack, VStack } from 'native-base'
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
 
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
@@ -28,6 +28,18 @@ export function Home(){
         my={10}
         maxH={'10'}
       />
+      
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize='md'>
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   )
 }

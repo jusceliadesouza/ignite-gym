a03c5448badbe261f914743491ef56ef24c2d565
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-20 23:22:21 -0300

    :lipstick: feat: estilização do evento de pressed

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { VStack } from 'native-base'
+import { HStack, VStack } from 'native-base'
 
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
@@ -8,7 +8,10 @@ export function Home(){
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="costas" />
+      <HStack>
+        <Group name="costa" />
+        <Group name="ombro" />
+      </HStack>
     </VStack>
   )
 }

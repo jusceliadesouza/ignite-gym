8ad7bd07b1163d62282319e86ef1828f9f39b95d
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-21 00:51:32 -0300

    :sparkles: feat: lista vazia

--- mobile/src/screens/Historic.tsx
+++ mobile/src/screens/Historic.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Heading, VStack, SectionList } from 'native-base'
+import { Heading, VStack, SectionList, Text } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { HistoryCard } from '@components/HistoryCard'
@@ -36,6 +36,17 @@ export function Historic(){
           </Heading>
         )}
         px={8}
+        contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+          <Text
+            color="gray.100"
+            textAlign="center"
+          >
+            Não há exercícios registrados ainda.{'\n'}
+            Bora se exercitar?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
       />
 
     </VStack>

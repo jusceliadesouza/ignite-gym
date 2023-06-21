a317a26d51cb21cffe713f39674348ce55bce0fd
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-21 00:40:55 -0300

    :sparkles: feat: agrupamento da listagem de dados com sectionlist

--- mobile/src/screens/Historic.tsx
+++ mobile/src/screens/Historic.tsx
@@ -1,14 +1,43 @@
-import { Center, Text, VStack } from 'native-base'
+import { useState } from 'react'
+import { Heading, VStack, SectionList } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { HistoryCard } from '@components/HistoryCard'
 
 export function Historic(){
+  const [exercises, setExercises] = useState([
+    {
+      title: '20.06.2023',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '21.06.2023',
+      data: ['Puxada frontal']
+    },
+  ])
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
 
-      <HistoryCard />
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading 
+            color="gray.200" 
+            fontSize="md" 
+            mt={10} 
+            mb={3}
+          >
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
+
     </VStack>
   )
 }

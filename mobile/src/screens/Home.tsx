4f0c6dc3228d4cc270eb9b6371415c0be3a458be
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-21 01:49:32 -0300

    :sparkles: feat: navegação para tela de exercício

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
@@ -8,7 +11,13 @@ import { HomeHeader } from '@components/HomeHeader'
 export function Home(){
   const [groups, setGroups] = useState(['costas', 'ombros', 'bíceps', 'tríceps'])
   const [groupSelected, setGroupSelected] = useState('costas')
-  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras'])
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento pernas'])
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise')
+  }
 
   return (
     <VStack flex={1}>
@@ -47,7 +56,9 @@ export function Home(){
           data={exercises}
           keyExtractor={item => item}
           renderItem={({ item }) => (
-            <ExerciseCard />
+            <ExerciseCard 
+              onPress={() => handleOpenExerciseDetails()}
+            />
           )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}

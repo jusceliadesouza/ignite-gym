4502ac6f2541f7027c06725c9c9280c9ff5426f0
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-26 17:39:41 -0300

    :sparkles: feat: validação da senha

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -18,8 +18,9 @@ type FormDataProps = {
 }
 
 const signUpSchema = yup.object({
-  name: yup.string().required('Informe o nome'),
-  email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+  name: yup.string().required('Informe o nome.'),
+  email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
+  password: yup.string().required('Informe a senha.').min(6, 'A senha deve ter, pelo menos, 6 caracteres.')
 })
 
 export function SignUp() {
@@ -77,7 +78,6 @@ export function SignUp() {
                 onChangeText={onChange}
                 value={value}
                 errorMessage={errors.name?.message}
-                isInvalid
               />
             )}
           />
@@ -105,6 +105,7 @@ export function SignUp() {
                 placeholder='Senha'
                 secureTextEntry
                 onChangeText={onChange}
+                errorMessage={errors.password?.message}
                 value={value}
               />
             )}

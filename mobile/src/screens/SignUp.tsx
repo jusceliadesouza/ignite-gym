759c266f97596417a689d7860d0bdbdbe43009f6
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-26 17:45:16 -0300

    :sparkles: feat: validação da confirmação de senha

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -20,7 +20,8 @@ type FormDataProps = {
 const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome.'),
   email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
-  password: yup.string().required('Informe a senha.').min(6, 'A senha deve ter, pelo menos, 6 caracteres.')
+  password: yup.string().required('Informe a senha.').min(6, 'A senha deve ter, pelo menos, 6 caracteres.'),
+  password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password'), null], 'A confirmação da senha não confere.')
 })
 
 export function SignUp() {
@@ -121,6 +122,7 @@ export function SignUp() {
                 onChangeText={onChange}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType='send'
+                errorMessage={errors.password_confirm?.message}
                 value={value}
               />
             )}

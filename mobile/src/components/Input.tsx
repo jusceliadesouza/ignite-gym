a654dc20eaf2aedfd795b39e632a45926fbceafe
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-26 18:03:54 -0300

    :lipstick: feat: revisão de código e refatorações

--- mobile/src/components/Input.tsx
+++ mobile/src/components/Input.tsx
@@ -30,7 +30,7 @@ export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
         {...rest}
       />
 
-      <FormControl.ErrorMessage>
+      <FormControl.ErrorMessage _text={{ color: "red.500" }}>
         {errorMessage}
       </FormControl.ErrorMessage>
     </FormControl>

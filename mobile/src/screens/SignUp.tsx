2c24156ef0ac0d260f72ca10f6326b1845cf2146
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-27 23:24:34 -0300

    :sparkles: feat: cadastrando o usuário

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -36,7 +36,14 @@ export function SignUp() {
   }
 
   function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm })
+    fetch('http://192.168.0.4:3333/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
   }
 
   return (

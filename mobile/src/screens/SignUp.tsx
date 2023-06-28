60a32e5b7bf82584aa95db0175a2450e36e44193
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-27 23:27:11 -0300

    :sparkles: feat: resposta do backend

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -44,6 +44,8 @@ export function SignUp() {
       },
       body: JSON.stringify({ name, email, password }),
     })
+      .then(response => response.json())
+      .then(data => console.log(data))
   }
 
   return (

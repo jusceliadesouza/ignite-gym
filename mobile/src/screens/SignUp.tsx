a8a39cf4d2ff15b42c10d46f44ad1a115d580d23
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-06-28 00:45:08 -0300

    :sparkles: feat: utilizando axios

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -4,6 +4,8 @@ import { useForm, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
+import { api } from '@services/api'
+
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImage from '@assets/background.png'
 
@@ -36,17 +38,8 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    const response = await fetch('http://192.168.0.4:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    })
-      
-      const data = await response.json()
-      console.log(data)
+    const response = await api.post('/users', { name, email, password })
+    console.log(response.data)
   }
 
   return (

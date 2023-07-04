0c09126c555105cf5da7604f410637a5d5d3f957
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-07-04 19:11:49 -0300

    :sparkles: feat: buscando dados do usuário no back-end

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,10 +1,11 @@
 import { ReactNode, createContext, useState } from "react";
 
+import { api } from '@services/api'
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO
-  signIn: (email: string, password: string) => void
+  signIn: (email: string, password: string) => Promise<void>
 }
 
 type AuthContextProviderProps = {
@@ -17,20 +18,19 @@ export const AuthContext = createContext<AuthContextDataProps>({
 } as AuthContextDataProps) 
 
 export function AuthContextProvider({ children }: AuthContextProviderProps ) {
-  const [user, setUser] = useState({
-    id: '1',
-    name: 'Juscélia de Souza',
-    email: 'jusceliadesouza@gmail.com',
-    avatar: 'juscelia.png'
-  })
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-  function signIn(email: string, password: string) {
-    setUser({
-      id: '',
-      name: '',
-      email,
-      avatar: "",
-    })
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password })
+  
+      if (data.user) {
+        setUser(data.user)
+      }
+    }
+    catch(error) {
+      throw error
+    }
   }
 
   return (
